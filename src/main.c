52a55bd25a1b23f11bf4ef7e10a31cc70eda8437
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-08-10 06:07:06 -0400

    Main: initialize and service ESP USART1

--- src/main.c
+++ src/main.c
@@ -5,26 +5,28 @@
 
 static void delay(uint32_t d)
 {
-    while(d--)
+    while (d--)
         __asm__("nop");
 }
 
 int main(void)
 {
     board_init();
-    uart4_init();
+    uart2_init();
+    esp_uart_init();
     cli_init();
 
-    uart4_print("\r\n");
-    uart4_print("---------------------------------\r\n");
-    uart4_print(" Directional Mic Controller\r\n");
-    uart4_print(" STM32F103RBT6\r\n");
-    uart4_print(" 72 MHz / USART2 PA2/PA3\r\n");
-    uart4_print(" CLI Ready\r\n");
-    uart4_print("---------------------------------\r\n");
-    uart4_print("> ");
+    uart2_print("\r\n");
+    uart2_print("---------------------------------\r\n");
+    uart2_print(" Directional Mic Controller\r\n");
+    uart2_print(" STM32F103RBT6\r\n");
+    uart2_print(" 72 MHz / USART2 PA2/PA3\r\n");
+    uart2_print(" ESP USART1 PA9/PA10\r\n");
+    uart2_print(" CLI Ready\r\n");
+    uart2_print("---------------------------------\r\n");
+    uart2_print("> ");
 
-    while(1)
+    while (1)
     {
         led_on();
         delay(100000);
@@ -33,5 +35,9 @@ int main(void)
         delay(100000);
 
         cli_task();
+
+        /* Pass any ESP-01 response to the debug UART. */
+        while (esp_uart_available())
+            uart2_putc(esp_uart_getc());
     }
 }
