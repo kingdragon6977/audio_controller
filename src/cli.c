ac64c83e2304d3a84bec7899a8ee560966240368
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-08-10 06:06:45 -0400

    CLI: use correctly named USART2 API and add ESP test command

--- src/cli.c
+++ src/cli.c
@@ -3,117 +3,112 @@
 #include "uart.h"
 #include "cli.h"
 #include <stdio.h>
+
 static char line[64];
-static int line_index=0;
+static int line_index = 0;
 
 static void execute(char *cmd)
 {
-    if(strcmp(cmd,"help")==0)
+    if (strcmp(cmd, "help") == 0)
     {
-        uart4_print("\r\nCommands:\r\n");
-        uart4_print(" help\r\n");
-        uart4_print(" id\r\n");
-        uart4_print(" uid\r\n");
-        uart4_print(" flash\r\n");
-        uart4_print(" ram\r\n");
-        uart4_print(" reboot\r\n");
-        uart4_print(" led on\r\n");
-        uart4_print(" led off\r\n");
+        uart2_print("\r\nCommands:\r\n");
+        uart2_print(" help\r\n");
+        uart2_print(" id\r\n");
+        uart2_print(" uid\r\n");
+        uart2_print(" flash\r\n");
+        uart2_print(" ram\r\n");
+        uart2_print(" reboot\r\n");
+        uart2_print(" led on\r\n");
+        uart2_print(" led off\r\n");
+        uart2_print(" esp test\r\n");
         return;
     }
 
-    if(strcmp(cmd,"id")==0)
+    if (strcmp(cmd, "id") == 0)
     {
-        uart4_print("STM32F103RBT6 MD\r\n");
+        uart2_print("STM32F103RBT6 MD\r\n");
         return;
     }
 
-    if(strcmp(cmd,"flash")==0)
+    if (strcmp(cmd, "flash") == 0)
     {
-        uart4_print("FLASH = 128 KB\r\n");
+        uart2_print("FLASH = 128 KB\r\n");
         return;
     }
 
-    if(strcmp(cmd,"ram")==0)
+    if (strcmp(cmd, "ram") == 0)
     {
-        uart4_print("RAM = 20 KB\r\n");
+        uart2_print("RAM = 20 KB\r\n");
         return;
     }
 
-    if(strcmp(cmd,"uid")==0)
+    if (strcmp(cmd, "uid") == 0)
     {
-        uint32_t *uid=(uint32_t*)0x1FFFF7E8;
+        uint32_t *uid = (uint32_t *)0x1FFFF7E8;
         char buf[80];
 
         sprintf(buf,
                 "%08lX %08lX %08lX\r\n",
-                uid[0],
-                uid[1],
-                uid[2]);
+                uid[0], uid[1], uid[2]);
 
-        uart4_print(buf);
+        uart2_print(buf);
         return;
     }
 
-    if(strcmp(cmd,"led on")==0)
+    if (strcmp(cmd, "led on") == 0)
     {
-        GPIO_ResetBits(GPIOB,GPIO_Pin_2);
-        uart4_print("LED ON\r\n");
+        GPIO_ResetBits(GPIOB, GPIO_Pin_2);
+        uart2_print("LED ON\r\n");
         return;
     }
 
-    if(strcmp(cmd,"led off")==0)
+    if (strcmp(cmd, "led off") == 0)
     {
-        GPIO_SetBits(GPIOB,GPIO_Pin_2);
-        uart4_print("LED OFF\r\n");
+        GPIO_SetBits(GPIOB, GPIO_Pin_2);
+        uart2_print("LED OFF\r\n");
         return;
     }
 
-    if(strcmp(cmd,"reboot")==0)
+    if (strcmp(cmd, "esp test") == 0)
+    {
+        esp_uart_print("AT\r\n");
+        uart2_print("ESP: AT sent\r\n");
+        return;
+    }
+
+    if (strcmp(cmd, "reboot") == 0)
     {
         NVIC_SystemReset();
     }
 
-    uart4_print("Unknown command\r\n");
+    uart2_print("Unknown command\r\n");
 }
 
 void cli_init(void)
 {
-    line_index=0;
-}
-
-int uart4_available(void)
-{
-    return (USART2->SR & USART_SR_RXNE);
-}
-
-char uart4_getc(void)
-{
-    return USART2->DR;
+    line_index = 0;
 }
 
 void cli_task(void)
 {
-    while(uart4_available())
+    while (uart2_available())
     {
-        char c=uart4_getc();
+        char c = uart2_getc();
 
-        if(c=='\r' || c=='\n')
+        if (c == '\r' || c == '\n')
         {
-            line[line_index]=0;
+            line[line_index] = 0;
 
-            uart4_print("\r\n");
-
+            uart2_print("\r\n");
             execute(line);
+            uart2_print("> ");
 
-            uart4_print("> ");
-
-            line_index=0;
+            line_index = 0;
         }
-        else if(line_index<63)
+        else if (line_index < 63)
         {
-            line[line_index++]=c;
-            uart4_putc(c);
+            line[line_index++] = c;
+            uart2_putc(c);
         }
     }
 }
