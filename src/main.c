cc5aec28c0bfa2edd0928a5cd1daf2b24b96a497
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-08-14 09:34:56 -0400

    Diagnostic: output HSE on PA8 MCO

--- src/main.c
+++ src/main.c
@@ -3,6 +3,23 @@
 #include "uart.h"
 #include "cli.h"
 
+/* Temporary HSE/MCO diagnostic: PA8 outputs the external HSE directly. */
+static void clock_test_mco_hse(void)
+{
+    GPIO_InitTypeDef gpio;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    gpio.GPIO_Pin   = GPIO_Pin_8;
+    gpio.GPIO_Mode  = GPIO_Mode_AF_PP;
+    gpio.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &gpio);
+
+    /* MCO = HSE. With the installed 8 MHz resonator this should be ~8 MHz. */
+    RCC->CFGR &= ~RCC_CFGR_MCO;
+    RCC->CFGR |= RCC_MCO_HSE;
+}
+
 static void delay(uint32_t d)
 {
     while (d--)
@@ -12,32 +29,15 @@ static void delay(uint32_t d)
 int main(void)
 {
     board_init();
-    uart2_init();
-    esp_uart_init();
-    cli_init();
 
-    uart2_print("\r\n");
-    uart2_print("---------------------------------\r\n");
-    uart2_print(" Directional Mic Controller\r\n");
-    uart2_print(" STM32F103RCT6\r\n");
-    uart2_print(" 72 MHz / USART2 PA2/PA3\r\n");
-    uart2_print(" ESP USART1 PA9/PA10\r\n");
-    uart2_print(" CLI Ready\r\n");
-    uart2_print("---------------------------------\r\n");
-    uart2_print("> ");
+    /* TEMPORARY CLOCK TEST: PA8 (MCO) = raw HSE. */
+    clock_test_mco_hse();
 
     while (1)
     {
         led_on();
         delay(100000);
-
         led_off();
         delay(100000);
-
-        cli_task();
-
-        /* Pass any ESP-01 response to the debug UART. */
-        while (esp_uart_available())
-            uart2_putc(esp_uart_getc());
     }
 }
