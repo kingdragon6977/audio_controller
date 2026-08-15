6ba72bd37d9f84830f92aee0c6641560a5a2185f
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-08-15 11:03:27 -0400

    I2C2: expose address-aware codec bus API

--- inc/i2c.h
+++ inc/i2c.h
@@ -3,15 +3,12 @@
 
 #include <stdint.h>
 
+#define TLV320ADC3101_I2C_ADDR 0x18u
+
 void i2c2_init(void);
 
-void i2c2_write(
-    uint8_t reg,
-    uint8_t data
-);
-
-uint8_t i2c2_read(
-    uint8_t reg
-);
+int i2c2_probe(uint8_t address);
+int i2c2_write(uint8_t address, uint8_t reg, uint8_t data);
+int i2c2_read(uint8_t address, uint8_t reg, uint8_t *data);
 
 #endif
