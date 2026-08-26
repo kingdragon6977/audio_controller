577baa3b9f356531149e63ecb43c858e736e9080
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-08-26 12:05:48 -0400

    I2S RX: expose peripheral and DMA debug snapshots

--- inc/i2s_rx.h
+++ inc/i2s_rx.h
@@ -5,10 +5,21 @@
 
 #define I2S_RX_SAMPLES 256u
 
+typedef struct
+{
+    uint32_t sr_before;
+    uint32_t sr_after_enable;
+    uint32_t sr_after_capture;
+    uint32_t dr_after_capture;
+    uint32_t dma_isr_after_capture;
+    uint16_t dma_cndtr_after_capture;
+} i2s_rx_debug_t;
+
 int i2s_rx_start_capture(void);
 void i2s_rx_stop(void);
 int i2s_rx_capture_complete(void);
 uint32_t i2s_rx_error_flags(void);
 const uint16_t *i2s_rx_buffer(void);
+void i2s_rx_get_debug(i2s_rx_debug_t *debug);
 
 #endif
