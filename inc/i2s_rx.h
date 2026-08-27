5ea6efc10a6776cc73f705dda6da696abae7647d
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-08-27 15:36:53 -0400

    I2S RX: expose frame-sync diagnostics and analysis

--- inc/i2s_rx.h
+++ inc/i2s_rx.h
@@ -13,6 +13,8 @@ typedef struct
     uint32_t dr_after_capture;
     uint32_t dma_isr_after_capture;
     uint16_t dma_cndtr_after_capture;
+    uint32_t ws_sync_ok;
+    uint32_t ws_level_at_enable;
 } i2s_rx_debug_t;
 
 int i2s_rx_start_capture(void);
@@ -21,5 +23,6 @@ int i2s_rx_capture_complete(void);
 uint32_t i2s_rx_error_flags(void);
 const uint16_t *i2s_rx_buffer(void);
 void i2s_rx_get_debug(i2s_rx_debug_t *debug);
+void i2s_rx_print_analysis(void);
 
 #endif
