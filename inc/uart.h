05648e27c857ca4386b9ec5eac268ece1ae11118
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-09-02 15:17:47 -0400

    USART1: expose binary ESP transport write

--- inc/uart.h
+++ inc/uart.h
@@ -1,6 +1,8 @@
 #ifndef UART_H
 #define UART_H
 
+#include <stdint.h>
+
 /* USART2 - primary/debug CLI on PA2/PA3 */
 void uart2_init(void);
 int uart2_available(void);
@@ -8,11 +10,12 @@ char uart2_getc(void);
 void uart2_putc(char c);
 void uart2_print(const char *s);
 
-/* USART1 - ESP-01 interface on PA9/PA10 */
+/* USART1 - ESP-01 high-speed transport on PA9/PA10 */
 void esp_uart_init(void);
 int esp_uart_available(void);
 char esp_uart_getc(void);
 void esp_uart_putc(char c);
+void esp_uart_write(const uint8_t *data, uint32_t length);
 void esp_uart_print(const char *s);
 
 #endif
